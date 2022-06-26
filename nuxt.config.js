26febb7d0fa90e0dff64718434b54fc2be5d081e
Hugo Larrousse <hugo.larrousse@gmail.com>
2022-06-26 18:32:02 +0700

    change favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,7 @@ export default {
     link: [
       // { rel: 'icon', type: 'image/x-icon', href: '/ballons.png' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Lato' }
     ]
   },
