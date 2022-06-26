dfa076386441d09cc08503f955a77d870eed0811
Hugo Larrousse <hugo.larrousse@gmail.com>
2022-06-26 17:54:31 +0700

    remove docs, add dist

---
+++ dist/_nuxt/static/1656240648/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Falexandra-quiz\u002F_nuxt\u002Fstatic\u002F1656240648",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Falexandra-quiz\u002F",assetsPath:"\u002Falexandra-quiz\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f9d789b0"}}}}(null));
